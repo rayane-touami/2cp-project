d9f07fbb36ff05a6c204e64d2e6251e1ec5df319
ritadj <ritadjhammane@gmail.com>
2026-04-23 11:36:03 +0100

    add universities models.py serializers.py url.

--- backend/features/universities/views.py
+++ backend/features/universities/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import University
+from .serializers import UniversitySerializer
 
-# Create your views here.
+class UniversityListView(generics.ListAPIView):
+    queryset = University.objects.all()
+    serializer_class = UniversitySerializer
+    permission_classes = []
