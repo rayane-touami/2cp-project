127ec6dbc5fbdf99bea2e3c4fde6c5c5596f0c42
nour <n.ouameurahmed@esi-sba.dz>
2026-04-25 16:39:37 +0200

    fix: disable browsable API in production

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -204,6 +204,9 @@ CHANNEL_LAYERS = {
 }
 
 REST_FRAMEWORK = {
+      'DEFAULT_RENDERER_CLASSES': [
+        'rest_framework.renderers.JSONRenderer',
+    ],
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework_simplejwt.authentication.JWTAuthentication',
