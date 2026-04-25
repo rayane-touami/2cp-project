0e1c9d85098adcb564e30ba6c0cc574b32369c08
nour <n.ouameurahmed@esi-sba.dz>
2026-04-25 15:01:01 +0200

    fix: remove django_extensions from INSTALLED_APPS for production

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -51,7 +51,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'django_extensions',
+    #'django_extensions',
         # Third-party
     'crispy_forms',
     'crispy_bootstrap5',
