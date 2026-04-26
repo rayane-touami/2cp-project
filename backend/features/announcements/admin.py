2b6ba4c7660391435ed1cea0433ecdb270af4619
ritadj <ritadjhammane@gmail.com>
2026-04-26 17:11:06 +0100

    add admin.py

--- backend/features/announcements/admin.py
+++ backend/features/announcements/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import Announcement, Photo
 
-# Register your models here.
+@admin.register(Announcement)
+class AnnouncementAdmin(admin.ModelAdmin):
+    list_display = ['title', 'student_full_name', 'status', 'created_at']
+    list_filter = ['status', 'category']
+    search_fields = ['title', 'student_full_name']
+
+@admin.register(Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    list_display = ['announcement', 'position']
