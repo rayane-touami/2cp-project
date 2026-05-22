1969716bd0789f9eb86c61f220f07360fb402738
ritadj <ritadjhammane@gmail.com>
2026-05-23 00:24:33 +0100

    add host to settings.py

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -20,6 +20,7 @@ ALLOWED_HOSTS = [
     '127.0.0.1', '10.0.2.2', 'localhost',
     'ritadjl.pythonanywhere.com',
     'twocp-project-mbil.onrender.com',
+    'twocp-project-1-gtam.onrender.com',
 ]
 CSRF_TRUSTED_ORIGINS = [
     'https://ritadjl.pythonanywhere.com',
