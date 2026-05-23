ae092b31c82ee5c07c0909d7a78c96f4af199793
ritadj <ritadjhammane@gmail.com>
2026-05-24 00:51:58 +0100

    fix

--- backend/build.sh
+++ backend/build.sh
@@ -5,4 +5,5 @@ mkdir -p static
 python manage.py collectstatic --no-input
 python manage.py migrate --run-syncdb
 python manage.py fix_university_column
-python manage.py loaddata categories.json || echo "Fixture already loaded, skipping"
+python manage.py loaddata categories.json || echo "categories already loaded"
+python manage.py loaddata universities.json || echo "universities already loaded"
