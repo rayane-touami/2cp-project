d21c1c779730b4e5e5375d6870c4138fa2877427
ritadj <ritadjhammane@gmail.com>
2026-05-24 00:14:14 +0100

    fix build command for render

--- backend/build.sh
+++ backend/build.sh
@@ -3,5 +3,5 @@ set -e
 pip install -r requirements.txt
 mkdir -p static
 python manage.py collectstatic --no-input
-python manage.py migrate
+python manage.py migrate --run-syncdb
 python manage.py fix_university_column
