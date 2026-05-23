522adfb615d4502b48317a96b80e66bb184c704f
ritadj <ritadjhammane@gmail.com>
2026-05-23 15:59:56 +0100

    remove loaddata from build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -4,5 +4,3 @@ pip install -r requirements.txt
 mkdir -p static
 python manage.py collectstatic --no-input
 python manage.py migrate --fake-initial
-python manage.py loaddata features/universities/fixtures/universities.json || echo "Universities already loaded, skipping"
-python manage.py loaddata features/announcements/fixtures/categories.json || echo "Categories already loaded, skipping"
