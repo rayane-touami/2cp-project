ddd9af847134564e2cc7658926ba69e8012f3ead
ritadj <ritadjhammane@gmail.com>
2026-05-24 21:35:48 +0100

    add command to build.sh

--- backend/build.sh
+++ backend/build.sh
@@ -5,5 +5,6 @@ mkdir -p static
 python manage.py collectstatic --no-input
 python manage.py migrate --run-syncdb
 python manage.py fix_university_column
+python manage.py fix_profiles
 python manage.py loaddata categories.json || echo "categories already loaded"
 python manage.py loaddata universities.json || echo "universities already loaded"
